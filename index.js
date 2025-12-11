73782cf4f28551e943ad4753240b7c0c41fb4eb8
GMRedoan <redoangazi69@gmail.com>
2025-12-12 05:41:35 +0600

    added profile update system

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
@@ -29,20 +29,56 @@ async function run() {
         const database = client.db('redCare_db')
         const districtsCollection = database.collection('districts')
         const upazilasCollection = database.collection('upazilas')
+        const usersCollection = database.collection('users')
 
         // get all districts/upazilas api
         app.get('/districts_upazilas', async (req, res) => {
-            const districts = await districtsCollection.find().project({id:1, name:1}).toArray()
-            const upazilas = await upazilasCollection.find().project({id:1, name:1}).toArray();
+            const districts = await districtsCollection.find().project({ id: 1, name: 1 }).toArray()
+            const upazilas = await upazilasCollection.find().project({ id: 1, name: 1 }).toArray();
             res.send({ districts, upazilas })
         })
 
+        // get users api
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
 
-        // app.post('/upazilas', async (req, res) => {
-        //     const newUpazilas = req.body
-        //     const result = await upazilasCollection.insertOne(newUpazilas)
-        //     res.send(result)
-        // })
+        app.get('/role/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await usersCollection.findOne(query)
+            res.send(result)
+        })
+
+
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body
+            const email = req.body.email
+            const query = { email: email }
+            const existingUser = await usersCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'already added this user in database' })
+            }
+            else {
+                const result = await usersCollection.insertOne(userInfo)
+                res.send(result)
+            }
+        })
+
+        // update the user information
+        app.patch('/users/:id', async (req, res) => {
+            const id = req.params.id
+            const updatedUser = req.body
+            delete updatedUser._id
+            const query =  { _id: new ObjectId(id) }
+            const update = {
+                $set: updatedUser
+            }
+            const result = await usersCollection.updateOne(query, update)
+            res.send(result)
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
